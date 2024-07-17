4aad5541fb09a548d3f7671b6025758cfdb9f20a
AshwinpillaiD <dashwinpillai@gmail.com>
2024-07-18 03:40:02 +0530

    Customise the Django Admin from  list_display,list_filter,search_fields,ordering,readonly_fields,list_editable

--- member/admin.py
+++ member/admin.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 from .models import Blog_commmad,Blogmodel
 # Register your models here.
 
+class BlogmodelAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'author', 'description')    # fields to display in the list view
+    list_filter  = ('author',)                          # add filters in the right sidebar
+    search_fields = ('author__username', )              # add search functionality 
+    # search_fields = ('author__username', )            # add search functionality And Here is an example using the first option, assuming author is a FOREIGNKEY to a User model and you want to search by the username:
+
+    ordering = ('description',)                         # default ordering of the list view
+    # ordering = ('name',)                              # default ordering by 'name' in ascending order
+    # ordering = ('-name',)                             # default ordering by 'name' in descending order
+    # ordering = ('author', 'name')                     # primary ordering by 'author', secondary ordering by 'name'
 
 
-admin.site.register(Blogmodel)
+    # fields = ('author',)                              # fields to display in the detail view
+    readonly_fields = ('author',)                       # fields to display as read-only
+
+    list_editable = ('description', )                   # fields that can be edited directly in the list view
+
+
+admin.site.register(Blogmodel,BlogmodelAdmin)
 admin.site.register(Blog_commmad)
 
 
