f086ccceaccf515a0597c73f0baaff64aeeebe02
AshwinpillaiD <dashwinpillai@gmail.com>
2024-07-18 01:46:46 +0530

    Customise the Django Admin from index_title,site_header,sit_title

--- member/admin.py
+++ member/admin.py
@@ -6,3 +6,9 @@ from .models import Blog_commmad,Blogmodel
 
 admin.site.register(Blogmodel)
 admin.site.register(Blog_commmad)
+
+
+admin.site.index_title="AP BLOG"
+admin.site.site_header="AP BLOG ADMIN"
+admin.site.site_title="AP BLOG TITLE"
+
